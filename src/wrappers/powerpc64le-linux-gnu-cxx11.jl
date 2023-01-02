cbc573d268c903b38ae105c9192357e79e6b8982
jlbuild <juliabuildbot@gmail.com>
2023-01-02 23:05:45 +0000

    FLAC_jll build 1.4.2+0

--- src/wrappers/powerpc64le-linux-gnu-cxx11.jl
+++ src/wrappers/powerpc64le-linux-gnu-cxx11.jl
@@ -2,13 +2,14 @@
 export flac, libflac, libflacpp, metaflac
 
 using Ogg_jll
+using CompilerSupportLibraries_jll
 JLLWrappers.@generate_wrapper_header("FLAC")
-JLLWrappers.@declare_library_product(libflac, "libFLAC.so.8")
-JLLWrappers.@declare_library_product(libflacpp, "libFLAC++.so.6")
+JLLWrappers.@declare_library_product(libflac, "libFLAC.so.12")
+JLLWrappers.@declare_library_product(libflacpp, "libFLAC++.so.10")
 JLLWrappers.@declare_executable_product(flac)
 JLLWrappers.@declare_executable_product(metaflac)
 function __init__()
-    JLLWrappers.@generate_init_header(Ogg_jll)
+    JLLWrappers.@generate_init_header(Ogg_jll, CompilerSupportLibraries_jll)
     JLLWrappers.@init_library_product(
         libflac,
         "lib/libFLAC.so",
