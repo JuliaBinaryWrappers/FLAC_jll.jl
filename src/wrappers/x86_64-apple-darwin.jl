23d92aa6abab4a60bfe1dce480e29e6f9cf8c8fb
jlbuild <juliabuildbot@gmail.com>
2023-01-03 12:53:05 +0000

    FLAC_jll build 1.3.4+2

--- src/wrappers/x86_64-apple-darwin.jl
+++ src/wrappers/x86_64-apple-darwin.jl
@@ -3,21 +3,21 @@ export flac, libflac, libflacpp, metaflac
 
 using Ogg_jll
 JLLWrappers.@generate_wrapper_header("FLAC")
-JLLWrappers.@declare_library_product(libflac, "@rpath/libFLAC.12.dylib")
-JLLWrappers.@declare_library_product(libflacpp, "@rpath/libFLAC++.10.dylib")
+JLLWrappers.@declare_library_product(libflac, "@rpath/libFLAC.8.dylib")
+JLLWrappers.@declare_library_product(libflacpp, "@rpath/libFLAC++.6.dylib")
 JLLWrappers.@declare_executable_product(flac)
 JLLWrappers.@declare_executable_product(metaflac)
 function __init__()
     JLLWrappers.@generate_init_header(Ogg_jll)
     JLLWrappers.@init_library_product(
         libflac,
-        "lib/libFLAC.12.dylib",
+        "lib/libFLAC.8.dylib",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
     JLLWrappers.@init_library_product(
         libflacpp,
-        "lib/libFLAC++.10.dylib",
+        "lib/libFLAC++.6.dylib",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
