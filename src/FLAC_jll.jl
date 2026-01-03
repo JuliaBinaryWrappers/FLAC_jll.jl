d1356b7f264d0b14e4407717f47dfee6405c8a4a
jlbuild <juliabuildbot@gmail.com>
2026-01-03 21:07:24 +0000

    FLAC_jll build 1.4.4+0

--- src/FLAC_jll.jl
+++ src/FLAC_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("FLAC")
-JLLWrappers.@generate_main_file("FLAC", UUID("1d38b3a6-207b-531b-80e8-c83f48dafa73"))
+JLLWrappers.@generate_main_file("FLAC", Base.UUID("1d38b3a6-207b-531b-80e8-c83f48dafa73"))
 end  # module FLAC_jll
